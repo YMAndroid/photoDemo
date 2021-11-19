036f8f76faef1358efa5938a4a00bfe6f7a71bb3
meng.yang_wh <meng.yang_wh@unistrong.com>
2021-11-19 18:01:20 +0800

    修改

--- pages/companyanniversary/companyanniversary.js
+++ pages/companyanniversary/companyanniversary.js
@@ -2,7 +2,7 @@
 // pages/guoqing/guoqing.js
 const ctx = wx.createCanvasContext('shareImg');
 const app = getApp();
-
+const windowWidth = wx.getSystemInfoSync().windowWidth;
 Page({
 
     /**
@@ -10,15 +10,25 @@ Page({
      */
     data: {
       prurl: '',
-  
       defaultImg: 0,
-  
       userInfo: {},
       hasUserInfo: false,
-  
       list: [
         0, 1, 2, 3, 4
-      ]
+      ],
+      scale:1,
+      rotate:0,
+      hat_center_x:0,
+      hat_center_x:0,
+      currentHatId:1,
+      picChoosed:false,
+      hatCenterX:wx.getSystemInfoSync().windowWidth/2,
+      hatCenterY:150,
+      cancelCenterX:wx.getSystemInfoSync().windowWidth/2-50-2,
+      cancelCenterY:100,
+      handleCenterX:wx.getSystemInfoSync().windowWidth/2+50-2,
+      handleCenterY:200,
+      hatSize:100,
     },
   
     selectImg: function(e){
@@ -40,6 +50,7 @@ Page({
           wx.showToast({
             title: '个性化',
           })
+          this.draw();
 
       } else {
         //简易操作
@@ -113,8 +124,7 @@ Page({
         that.drawImg(that.data.userInfo.avatarUrl);
       }
   
-    },
-    
+    },   
   
     drawImg(avatarUrl){
       let that = this;
@@ -226,10 +236,128 @@ Page({
       }
     },
   
-    /**
-     * 用户点击右上角分享朋友圈
-     */
-    onShareTimeline(){
+    onReady(){
+      this.hat_center_x=this.data.hatCenterX;
+      this.hat_center_y=this.data.hatCenterY;
+      this.cancel_center_x=this.data.cancelCenterX;
+      this.cancel_center_y=this.data.cancelCenterY;
+      this.handle_center_x=this.data.handleCenterX;
+      this.handle_center_y=this.data.handleCenterY;
+  
+      this.scale=this.data.scale;
+      this.rotate=this.data.rotate;
       
-    }
+      this.touch_target="";
+      this.start_x=0;
+      this.start_y=0;
+    },
+    
+    touchStart(e){
+      if(e.target.id=="hat"){
+        this.touch_target="hat";
+      }else if(e.target.id=="handle"){
+        this.touch_target="handle"
+      }else{
+        this.touch_target=""
+      };
+      
+      if(this.touch_target!=""){
+        this.start_x=e.touches[0].clientX;
+        this.start_y=e.touches[0].clientY;
+      }
+    },
+    touchEnd(e){
+        this.hat_center_x=this.data.hatCenterX;
+        this.hat_center_y=this.data.hatCenterY;
+        this.cancel_center_x=this.data.cancelCenterX;
+        this.cancel_center_y=this.data.cancelCenterY;
+        this.handle_center_x=this.data.handleCenterX;
+        this.handle_center_y=this.data.handleCenterY;
+      // }
+      this.touch_target="";
+      this.scale=this.data.scale;
+      this.rotate=this.data.rotate;
+    },
+    touchMove(e){
+        var current_x=e.touches[0].clientX;
+        var current_y=e.touches[0].clientY;
+        var moved_x=current_x-this.start_x;
+        var moved_y=current_y-this.start_y;
+        if(this.touch_target=="hat"){
+          this.setData({
+            hatCenterX:this.data.hatCenterX+moved_x,
+            hatCenterY:this.data.hatCenterY+moved_y,
+            cancelCenterX:this.data.cancelCenterX+moved_x,
+            cancelCenterY:this.data.cancelCenterY+moved_y,
+            handleCenterX:this.data.handleCenterX+moved_x,
+            handleCenterY:this.data.handleCenterY+moved_y
+          })
+        };
+        if(this.touch_target=="handle"){
+          this.setData({
+            handleCenterX:this.data.handleCenterX+moved_x,
+            handleCenterY:this.data.handleCenterY+moved_y,
+            cancelCenterX:2*this.data.hatCenterX-this.data.handleCenterX,
+            cancelCenterY:2*this.data.hatCenterY-this.data.handleCenterY
+          });
+          let diff_x_before=this.handle_center_x-this.hat_center_x;
+          let diff_y_before=this.handle_center_y-this.hat_center_y;
+          let diff_x_after=this.data.handleCenterX-this.hat_center_x;
+          let diff_y_after=this.data.handleCenterY-this.hat_center_y;
+          let distance_before=Math.sqrt(diff_x_before*diff_x_before+diff_y_before*diff_y_before);
+          let distance_after=Math.sqrt(diff_x_after*diff_x_after+diff_y_after*diff_y_after);
+          let angle_before=Math.atan2(diff_y_before,diff_x_before)/Math.PI*180;
+          let angle_after=Math.atan2(diff_y_after,diff_x_after)/Math.PI*180;
+          this.setData({
+            scale:distance_after/distance_before*this.scale,
+            rotate:angle_after-angle_before+this.rotate,
+          })
+        }
+        this.start_x=current_x;
+        this.start_y=current_y;
+    },
+
+    
+  draw() {
+    wx.getImageInfo({
+      src:this.data.userInfo.avatarUrl,
+      success: res => {
+          this.bgPic = res.path;
+          const hat_size = 100 * this.data.scale;
+          ctx.clearRect(0, 0, 250, 250);
+          console.log("windowWidth:",windowWidth);
+          ctx.drawImage(this.bgPic, 0, 0, 250, 250);
+          ctx.translate(this.data.hatCenterX,this.data.hatCenterY);
+          ctx.rotate(this.data.rotate * Math.PI / 180);
+          ctx.drawImage(`../../images/hat${this.data.defaultImg}.png`, -hat_size / 2, -hat_size / 2, hat_size, hat_size);
+          ctx.draw();
+
+
+          wx.canvasToTempFilePath({
+            x: windowWidth / 2 - 150,
+            y: 0,
+            height: 300,
+            width: 300,
+            canvasId: 'myCanvas',
+            success: (res) => {
+              wx.saveImageToPhotosAlbum({
+                filePath: res.tempFilePath,
+                success: (res) => {
+                  this.setData({
+                    isSave: false
+                  })
+                  wx.showToast({
+                    title: '图片已保存到相册，请前往微信去设置头像!',
+                  })
+                  console.log("success:" + res);
+                }, fail(e) {
+                  console.log("err:" + e);
+                }
+              })
+            }
+          });
+      }
+    })
+
+  },
   })
